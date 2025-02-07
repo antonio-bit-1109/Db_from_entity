a0be050e1115d096bc0f46453ffd14901d461e1f
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-02-07 15:32:55 +0100

    first commit prova creazione db partendo da entity

---
+++ src/main/java/org/example/provadbentity/ProvaDbEntityApplication.java
@@ -0,0 +1,13 @@
+package org.example.provadbentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaDbEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaDbEntityApplication.class, args);
+    }
+
+}
